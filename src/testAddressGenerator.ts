ddb0c4a7ea94e991f587012fb32e9d251c13ead9
visionsofparadise@pm.me <visionsofparadise@pm.me>
2021-03-31 17:12:58 +0100

    fix

--- src/testAddressGenerator.ts
+++ src/testAddressGenerator.ts
@@ -1,4 +1,4 @@
-import { HDPublicKey, Address, Networks } from 'bitcore-lib-ltc'
+import { HDPublicKey, Address, Networks } from 'litecore-lib'
 
 export const testAddressGenerator = (n?: number) => {
 	const xPubKeyObj = new HDPublicKey(process.env.TEST_XPUBKEY!);
